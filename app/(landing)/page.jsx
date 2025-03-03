5f56d3452442b62fad03cd1e2a18d3c34118e859
404Nikhil <nicksd111@gmail.com>
2025-03-04 02:04:09 +0530

    footer

--- app/(landing)/page.jsx
+++ app/(landing)/page.jsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-    <div>
-      
+    <div className="min-h-screen bg-dark text-white">
+      <Navbar />
+      <Footer />
     </div>
   );
 }
